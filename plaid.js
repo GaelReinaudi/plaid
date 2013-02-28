71024f80a96f4b390e19c3b7a47c1d2fa34edaa2
gael <gael@ubuntu.ubuntu-domain>
2013-02-28 12:04:11 -0500

    click click click

--- plaid.js
+++ plaid.js
@@ -1,15 +1,46 @@
+Panels = new Meteor.Collection('panels');
+
 if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to plaid.";
+	
+	Template.panels.panels = function() {
+		return Panels.find({}, {})
+	}
+
+	Template.hello.events({
+		'click input' : function () {
+			// template data, if any, is available in 'this'
+			if (typeof console !== 'undefined')
+				console.log("You pressed the button!");
+			Panels.insert({name: "ffffffff", x: 240, y: 240});
+			} 
+	});
+	
+	Template.panel.events({
+		'mousemove' : function () {
+			// template data, if any, is available in 'this'
+			if (typeof console !== 'undefined')
+				console.log("You moved over the div:" + this._id);
+			var Speed = 10;
+			var randX = (Math.random() - 0.48) * Speed * 2;
+			var randY = (Math.random() - 0.48) * Speed * 2;
+			Panels.update({_id: this._id}, {$inc: {x: randX}});
+			Panels.update({_id: this._id}, {$inc: {y: randY}});			
+			},
+		'click div' : function () {
+			if (typeof console !== 'undefined')
+				console.log("You clicked the div:" + this._id);
+			Panels.remove({_id: this._id});
+		}
+		
+	});
+	
+	
+  Template.theTitle.greeting = function () {
+	window.innerWidth;
+    return "plaid" + " " + window.innerWidth;
   };
 
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
-  });
+
 }
 
 if (Meteor.isServer) {
